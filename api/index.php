3dc5f15f3668c7d5335045b965523ac5da827db2
ushchent <am@opendata.by>
2016-05-10 17:23:54 +0300

    Добавить API, таблицу с областными бюджетами и графики с исполнением.

---
+++ api/index.php
@@ -0,0 +1,243 @@
+<?php
+	// Выдаем json. Потом сделаем на этой странице API.
+	if (isset($_GET['region']) and isset($_GET['function'])) {
+
+		$region = ($_GET['region']);
+		$function = ($_GET['function']);
+    $db = new SQLite3('../data/budgety.sqlite');
+    //echo "Ok<br>";
+function sort_table($a, $b) {
+	return $b['percent'] - $a['percent'];
+}
+
+function move_total($array_name) {
+	for ($i = 0; $i < count($array_name); $i++) {
+		if ($array_name[$i]['title'] == "ВСЕГО расходов") {
+			$totals = $array_name[$i];
+			array_splice($array_name, $i, 1);
+		}
+	};
+	array_push($array_name, $totals);
+	return $array_name;
+}
+
+
+//echo $region . " - " . $function;
+
+    if ($region == "default" and $function == "default") {
+	
+	// Вывод для всех функций без указания регионов. Это первая таблица по умолчанию.
+	$sql_total_plan_data = "select title, sum(amount) as plan from raskhody_f where status = 'П' and code_2 = 0 group by title";
+		
+	$sql_total_popravki_data = 'select title, sum(amount) as changed from (select title, amount from raskhody_f where (region = "Могилевская область" and status = "У2" and code_2 = 0) or (region = "Минская область" and status = "У2" and code_2 = 0) or (region = "Витебская область" and status = "У1" and code_2 = 0) or (region = "Брестская область" and status = "У1" and code_2 = 0) or (region = "Гродненская область" and status = "У1" and code_2 = 0) or (region = "Минск" and status = "У1" and code_2 = 0) or (region = "Гомельская область" and status = "У1" and code_2 = 0)) group by title;';
+	
+	$total_plan_data = $db->query($sql_total_plan_data);
+	$total_popravki_data = $db->query($sql_total_popravki_data);
+	
+	$total_result_set = array();
+	$row_array = array();
+
+	while($row = $total_plan_data->fetchArray(SQLITE3_ASSOC)) {
+		$row_array['title'] = $row["title"];
+		$row_array['plan'] = $row["plan"];
+		array_push($total_result_set, $row_array);
+	}
+
+	$total_popravki_set = array();
+	while ($row = $total_popravki_data->fetchArray(SQLITE3_ASSOC)) {
+		$temp_arr = array();
+		$temp_arr['title'] = $row['title'];
+		$temp_arr['changed'] = $row['changed'];
+		array_push($total_popravki_set, $temp_arr); 
+	}
+
+	for ($i = 0; $i < count($total_result_set); $i++) {
+		for ($b = 0; $b < count($total_popravki_set); $b++) {
+			if ($total_result_set[$i]['title'] == $total_popravki_set[$b]['title']) {
+				$total_result_set[$i]['changed'] = $total_popravki_set[$b]['changed'];
+				$total_result_set[$i]["percent"] = 
+					number_format(($total_result_set[$i]['changed'] - $total_result_set[$i]['plan']) / 
+					$total_result_set[$i]['plan'] * 100, $decimals = 1);
+			}
+		}
+	}
+
+
+usort($total_result_set, "sort_table");
+
+$output = move_total($total_result_set);
+echo json_encode($output);
+ //  <select onchange="myFunction()">
+
+
+
+    
+} else if ($region != "default" and $function == "default") {
+	// Вывод по указанному региону для всех видов расходов
+	$sql_region_plan = 'select region, title, amount as plan from raskhody_f inner join regions on regions.region_name = raskhody_f.region where region_id = ' . $region . ' and status = "П" and code_2 = 0;';
+	
+
+	$sql_region_last_popravka = 'select distinct subject, status from popravki_o inner join regions on regions.region_name = popravki_o.region where region_id = ' . $region . ' group by subject order by status;';
+	
+	$region_last_popravka = $db->query($sql_region_last_popravka);
+	$popravka = $region_last_popravka->fetchArray()['status'];
+
+	$sql_region_changed = 'select title, amount as changed from raskhody_f inner join regions on regions.region_name = raskhody_f.region where region_id = ' . $region . ' and status = "' . $popravka . '" and code_2 = 0;';
+	
+	
+
+	$region_plan_data = $db->query($sql_region_plan);
+	$region_changed_data = $db->query($sql_region_changed);
+
+
+	$result_set = array();
+	$row_array = array();
+
+	while($row = $region_plan_data->fetchArray()) {
+		$row_array['title'] = $row["title"];
+		$row_array['plan'] = $row["plan"];
+		array_push($result_set, $row_array);
+	}
+
+	$region_changed_set = array();
+	while ($row = $region_changed_data->fetchArray(SQLITE3_ASSOC)) {
+		$temp_arr = array();
+		$temp_arr['title'] = $row['title'];
+		$temp_arr['changed'] = $row['changed'];
+		array_push($region_changed_set, $temp_arr); 
+	}
+
+	for ($i = 0; $i < count($result_set); $i++) {
+		for ($b = 0; $b < count($region_changed_set); $b++) {
+			if ($result_set[$i]['title'] == $region_changed_set[$b]['title']) {
+				$result_set[$i]['changed'] = $region_changed_set[$b]['changed'];
+				$result_set[$i]["percent"] = 
+					number_format(($result_set[$i]['changed'] - $result_set[$i]['plan']) / 
+					$result_set[$i]['plan'] * 100, $decimals = 1);
+			}
+		}
+	}
+usort($result_set, "sort_table");
+
+$output = move_total($result_set);
+echo json_encode($output);
+
+} else if ($region != "default" and $function != "default"){
+	// вывод для всех регионов по указанной функции
+	// желательно сбросить селектор регионов - на JS?
+	
+	// Определяем функцию
+
+	$sql_function_plan = 'select region, amount as plan, function_id  from raskhody_f inner join functions on functions.function = raskhody_f.title where function_id = ' . $function . ' and status = "П" and code_2 = 0;';
+
+	$sql_function_title = 'select function from functions where function_id = ' . $function . ';';
+
+	$get_function_title = $db->query($sql_function_title);
+	$function_title = $get_function_title->fetchArray()['function'];
+	
+
+	
+	$sql_function_changed = 'select title, amount as changed, region from (select title, amount, region from raskhody_f where (region = "Могилевская область" and status = "У2" and code_2 = 0) or (region = "Минская область" and status = "У2" and code_2 = 0) or (region = "Витебская область" and status = "У1" and code_2 = 0) or (region = "Брестская область" and status = "У1" and code_2 = 0) or (region = "Гродненская область" and status = "У1" and code_2 = 0) or (region = "Минск" and status = "У1" and code_2 = 0) or (region = "Гомельская область" and status = "У1" and code_2 = 0)) where title = "' . $function_title . '" group by region;';
+	
+
+	
+	$function_plan_data = $db->query($sql_function_plan);
+	
+	$function_changed_data = $db->query($sql_function_changed);
+	
+
+	
+	$function_set = array();
+	$row_array = array();
+
+	while($row = $function_plan_data->fetchArray(SQLITE3_ASSOC)) {
+		$row_array['region'] = $row["region"];
+		$row_array['plan'] = $row["plan"];
+		array_push($function_set, $row_array);
+	}
+
+	$function_changed_set = array();
+	while ($row = $function_changed_data->fetchArray(SQLITE3_ASSOC)) {
+		$temp_arr = array();
+		$temp_arr['region'] = $row['region'];
+		$temp_arr['changed'] = $row['changed'];
+		array_push($function_changed_set, $temp_arr); 
+	}
+
+	for ($i = 0; $i < count($function_set); $i++) {
+		for ($b = 0; $b < count($function_changed_set); $b++) {
+			if ($function_set[$i]['region'] == $function_changed_set[$b]['region']) {
+				$function_set[$i]['changed'] = $function_changed_set[$b]['changed'];
+				$function_set[$i]["percent"] = 
+					number_format(($function_set[$i]['changed'] - $function_set[$i]['plan']) / 
+					$function_set[$i]['plan'] * 100, $decimals = 1);
+			}
+		}
+	};
+usort($function_set, "sort_table");
+
+
+
+echo json_encode($function_set);
+
+} else if ($region == "default" and $function != "default") {
+$sql_function_plan = 'select region, amount as plan, function_id  from raskhody_f inner join functions on functions.function = raskhody_f.title where function_id = ' . $function . ' and status = "П" and code_2 = 0;';
+
+	$sql_function_title = 'select function from functions where function_id = ' . $function . ';';
+
+	$get_function_title = $db->query($sql_function_title);
+	$function_title = $get_function_title->fetchArray()['function'];
+	
+
+	
+	$sql_function_changed = 'select title, amount as changed, region from (select title, amount, region from raskhody_f where (region = "Могилевская область" and status = "У2" and code_2 = 0) or (region = "Минская область" and status = "У2" and code_2 = 0) or (region = "Витебская область" and status = "У1" and code_2 = 0) or (region = "Брестская область" and status = "У1" and code_2 = 0) or (region = "Гродненская область" and status = "У1" and code_2 = 0) or (region = "Минск" and status = "У1" and code_2 = 0) or (region = "Гомельская область" and status = "У1" and code_2 = 0)) where title = "' . $function_title . '" group by region;';
+	
+
+	
+	$function_plan_data = $db->query($sql_function_plan);
+	
+	$function_changed_data = $db->query($sql_function_changed);
+	
+
+	
+	$function_set = array();
+	$row_array = array();
+
+	while($row = $function_plan_data->fetchArray(SQLITE3_ASSOC)) {
+		$row_array['region'] = $row["region"];
+		$row_array['plan'] = $row["plan"];
+		array_push($function_set, $row_array);
+	}
+
+	$function_changed_set = array();
+	while ($row = $function_changed_data->fetchArray(SQLITE3_ASSOC)) {
+		$temp_arr = array();
+		$temp_arr['region'] = $row['region'];
+		$temp_arr['changed'] = $row['changed'];
+		array_push($function_changed_set, $temp_arr); 
+	}
+
+	for ($i = 0; $i < count($function_set); $i++) {
+		for ($b = 0; $b < count($function_changed_set); $b++) {
+			if ($function_set[$i]['region'] == $function_changed_set[$b]['region']) {
+				$function_set[$i]['changed'] = $function_changed_set[$b]['changed'];
+				$function_set[$i]["percent"] = 
+					number_format(($function_set[$i]['changed'] - $function_set[$i]['plan']) / 
+					$function_set[$i]['plan'] * 100, $decimals = 1);
+			}
+		}
+	};
+usort($function_set, "sort_table");
+
+echo json_encode($function_set);
+} else {
+
+
+
+    //$db->close();
+		
+	echo "Unknown query.";
+	}
+	 
+}
+?>
--- js/script.js
+++ js/script.js
@@ -1,243 +1,291 @@
+function addCommas(numberString) {
+	numberString += '';
+	x = numberString.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+}
+
 var xScale = d3.scale.linear()
                 .range([0, 600]);
-var bsgu = d3.select("#bsgu")
-			.append("svg")
-			.attr({
-				width: 800,
-				height: 120
-			});
-
-var resp = d3.select("#resp")
-			.append("svg")
-			.attr({
-				width: 800,
-				height: 120
-			});
-
-var fszn = d3.select("#fszn")
-			.append("svg")
-			.attr({
-				width: 800,
-				height: 120
-			});
+                
+var yScale = d3.scale.linear()
+                .range([0, 60]);
 
 
-d3.json("data/data_2016.json", function(data) {
-    var bsguSpending = data.filter(function(d) { return d.title == "БСГУ" && d.type == "Расходы"});
-  var bsguRevenue = data.filter(function(d) { return d.title == "БСГУ" && d.type == "Доходы"; });
-	var respSpending = data.filter(function(d) { return d.title == "Республиканский" && d.type == "Расходы"; });
-  var respRevenue = data.filter(function(d) { return d.title == "Республиканский" && d.type == "Доходы"; });
-    var fsznRevenue = data.filter(function(d) { return d.title == "ФСЗН" && d.type == "Доходы"; });
-var fsznSpending = data.filter(function(d) { return d.title == "ФСЗН" && d.type == "Расходы"; });
+var bsgu_rev = d3.select("#bsgu_rev")
+            .append("svg")
+            .attr({
+                width: 800,
+                height: 40
+            });
+
+var bsgu_spent = d3.select("#bsgu_spent")
+            .append("svg")
+            .attr({
+                width: 800,
+                height: 40
+            });
+
+var resp_rev = d3.select("#resp_rev")
+            .append("svg")
+            .attr({
+                width: 800,
+                height: 40
+            });
+var resp_spent = d3.select("#resp_spent")
+            .append("svg")
+            .attr({
+                width: 800,
+                height: 40
+            });
+
+d3.json("data/data.json", function(data) {
+	var bsgu_rev_data = data.filter(function(d) { return d.type == "Всего" && d.title == "Всего доходов"; });
+	var bsgu_spent_data = data.filter(function(d) { return d.type == "Всего" && d.title == "Всего расходов"; });
+	var resp_rev_data = data.filter(function(d) { return d.type == "Республиканский" && d.title == "Всего доходов"; });
+	var resp_spent_data = data.filter(function(d) { return d.type == "Республиканский" && d.title == "Всего расходов"; });
+	
+	xScale.domain([0, d3.max(data, function(d) { return d.amount})]);
 
 // БСГУ
-    xScale.domain([0, bsguRevenue[0].amount])
-    bsgu.append("rect")
-        .attr({
-            x: 90,
-            y: 10,
-            width: xScale(bsguRevenue[0].amount),
-            height: 30,
-            fill: "#F46A60",
-            opacity: .8,
-            title: "План: " + bsguRevenue[0].amount,
-    });
-    bsgu.append("rect")
-        .attr({
-            x: 90,
-            y: 10,
-            width: xScale(bsguRevenue[1].amount),
-            height: 30,
-            fill: "#2A2F4E",
-            opacity: .9,
-            title: "Исполнено: " + bsguRevenue[1].amount
-    })
-    bsgu.append("text")
-        .attr({
-            x: 0,
-            y: 30 
-        })
-        .text("Доходы");
-    bsgu.append("text")
-        .attr({
-            x: 0,
-            y: 70
-        })
-        .text("Расходы");
-    bsgu.append("text")
-        .attr({
-            x: 0,
-            y: 110
-        })
-        .text("Долг");
-    bsgu.append("rect")
-        .attr({
-            x: 90,
-            y: 50,
-            width: xScale(bsguSpending[0].amount),
-            height: 30,
-            fill: "#F46A60",
-            opacity: .7,
-            title: "План: " + bsguSpending[0].amount
-    });
-    bsgu.append("rect")
-        .attr({
-            x: 90,
-            y: 50,
-            width: xScale(bsguSpending[1].amount),
-            height: 30,
-            fill: "#2A2F4E",
-            opacity: .9,
-            title: "Исполнено: " + bsguSpending[1].amount
-    });
-    bsgu.append("rect")
-        .attr({
-            x: 90,
-            y: 90,
-            width: xScale(379000),
-            height: 20,
-            fill: "black",
-            opacity: .6,
-            title: "379000"
-    });
-    
+bsgu_rev.selectAll("rect")
+		.data(bsgu_rev_data)
+		.enter()
+		.append("rect")
+		.attr({
+			x: 80,
+			y: 0,
+			width: function(d) { return xScale(d.amount); },
+			height: 40,
+			fill: function(d) { return (d.status == "УП") ?  "#F46A60" : "#2A2F4E"; },
+			title: function(d) { return d.amount; }
+		});
+
+bsgu_rev.selectAll("text")
+	.data(bsgu_rev_data)
+	.enter()
+	.append("text")
+	.attr({
+		x: function(d) { return (d.status == "УП") ?  (xScale(d.amount) - 60) : 85; },
+		y: 25,
+		fill: "white",
+		opacity: .9
+	})
+	.text(function(d) {return (d.status == "УП") ? "План: " +
+	addCommas(d.amount) : addCommas(d.amount); });
+
+	bsgu_spent.selectAll("rect")
+		.data(bsgu_spent_data)
+		.enter()
+		.append("rect")
+		.attr({
+			x: 80,
+			y: 0,
+			width: function(d) { return xScale(d.amount); },
+			height: 40,
+			fill: function(d) { return (d.status == "УП") ?  "#F46A60" : "#2A2F4E"; },
+			title: function(d) { return d.amount; }
+		});
+
+	bsgu_spent.selectAll("text")
+	.data(bsgu_spent_data)
+	.enter()
+	.append("text")
+	.attr({
+		x: function(d) { return (d.status == "УП") ?
+		(xScale(d.amount) - 60) : 85; },
+		y: 25,
+		fill: "white",
+		opacity: .9
+	})
+	.text(function(d) { return (d.status == "УП") ? "План: " +
+	addCommas(d.amount) : addCommas(d.amount); });
+
+bsgu_rev.append("text")
+		.attr({
+			x: 0,
+			y: 25
+			})
+		.text("Доходы");
+bsgu_spent.append("text")
+		.attr({
+			x: 0,
+			y: 25
+			})
+		.text("Расходы");
+
 // Республиканский
+resp_rev.selectAll("rect")
+		.data(resp_rev_data)
+		.enter()
+		.append("rect")
+		.attr({
+			x: 80,
+			y: 0,
+			width: function(d) { return xScale(d.amount); },
+			height: 40,
+			fill: function(d) { return (d.status == "УП") ?  "#F46A60" : "#2A2F4E"; },
+			title: function(d) { return d.amount; }
+		});
 
-    resp.append("rect")
-        .attr({
-            x: 90,
-            y: 10,
-            width: xScale(respRevenue[0].amount),
-            height: 30,
-            fill: "#F46A60",
-            opacity: .8,
-            title: "План: " + respRevenue[0].amount
-    });
-    resp.append("rect")
-        .attr({
-            x: 90,
-            y: 10,
-            width: xScale(respRevenue[1].amount),
-            height: 30,
-            fill: "#2A2F4E",
-            opacity: .9,
-            title: "Исполнено: " + respRevenue[1].amount
-    });
-    resp.append("text")
-        .attr({
-            x: 0,
-            y: 30 
-        })
-        .text("Доходы");
-    resp.append("text")
-        .attr({
-            x: 0,
-            y: 70
-        })
-        .text("Расходы");
-    //resp.append("text")
-        //.attr({
-            //x: 0,
-            //y: 110
-        //})
-        //.text("Долг");
-    resp.append("rect")
-        .attr({
-            x: 90,
-            y: 50,
-            width: xScale(respSpending[0].amount),
-            height: 30,
-            fill: "#F46A60",
-            opacity: .7,
-            title: "План: " + respSpending[0].amoun
-    });
-    resp.append("rect")
-        .attr({
-            x: 90,
-            y: 50,
-            width: xScale(respSpending[1].amount),
-            height: 30,
-            fill: "#2A2F4E",
-            opacity: .9,
-            title: "Исполнено: " + respSpending[1].amount
-    });
-    //resp.append("rect")
-        //.attr({
-            //x: 90,
-            //y: 90,
-            //width: xScale(379000),
-            //height: 20,
-            //fill: "#F46A60",
-            //opacity: .5
-    //});
+resp_rev.selectAll("text")
+	.data(resp_rev_data)
+	.enter()
+	.append("text")
+	.attr({
+		x: function(d) { return (d.status == "УП") ?  (xScale(d.amount) - 50) : 85; },
+		y: 25,
+		fill: "white",
+		"font-size": function(d) { return (d.status == "Исполнено") ?  "9px" : "14px"; },
+		opacity: .9
+	})
+	.text(function(d) { return (d.status == "УП") ? "План: " +
+	addCommas(d.amount) : addCommas(d.amount); });
 
-// ФСЗН
+	resp_spent.selectAll("rect")
+		.data(resp_spent_data)
+		.enter()
+		.append("rect")
+		.attr({
+			x: 80,
+			y: 0,
+			width: function(d) { return xScale(d.amount); },
+			height: 40,
+			fill: function(d) { return (d.status == "УП") ?  "#F46A60" : "#2A2F4E"; },
+			title: function(d) { return d.amount; }
+		});
 
-    fszn.append("rect")
-        .attr({
-            x: 90,
-            y: 10,
-            width: xScale(fsznRevenue[0].amount),
-            height: 30,
-            fill: "#F46A60",
-            opacity: .8,
-            title: "План: " + fsznRevenue[0].amount
-    });
-    fszn.append("rect")
-        .attr({
-            x: 90,
-            y: 10,
-            width: xScale(fsznRevenue[1].amount),
-            height: 30,
-            fill: "#2A2F4E",
-            opacity: .9,
-            title: "Исполнено: " + fsznRevenue[1].amount
-    });
-    fszn.append("text")
-        .attr({
-            x: 0,
-            y: 30 
-        })
-        .text("Доходы");
-    fszn.append("text")
-        .attr({
-            x: 0,
-            y: 70
-        })
-        .text("Расходы");
-    //fszn.append("text")
-        //.attr({
-            //x: 0,
-            //y: 110
-        //})
-        //.text("Долг");
-    fszn.append("rect")
-        .attr({
-            x: 90,
-            y: 50,
-            width: xScale(fsznSpending[0].amount),
-            height: 30,
-            fill: "#F46A60",
-            opacity: .7,
-            title: "План: " + fsznSpending[0].amount
-    });
-    //fszn.append("rect")
-        //.attr({
-            //x: 90,
-            //y: 50,
-            //width: xScale(respSpending[1].amount),
-            //height: 30,
-            //fill: "#2A2F4E",
-            //opacity: .9
-    //});
-    //fszn.append("rect")
-        //.attr({
-            //x: 90,
-            //y: 90,
-            //width: xScale(379000),
-            //height: 20,
-            //fill: "#F46A60",
-            //opacity: .5
-    //});
+resp_spent.selectAll("text")
+	.data(resp_spent_data)
+	.enter()
+	.append("text")
+	.attr({
+		x: function(d) { return (d.status == "УП") ?  (xScale(d.amount) - 50) : 85; },
+		y: 25,
+		fill: "white",
+		"font-size": function(d) { return (d.status == "Исполнено") ?  "9px" : "14px"; },
+		opacity: .9
+	})
+	.text(function(d) { return (d.status == "УП") ? "План: " +
+	addCommas(d.amount) : addCommas(d.amount); });
 
-});
+
+	resp_rev.append("text")
+		.attr({
+			x: 0,
+			y: 25
+			})
+		.text("Доходы");
+
+	resp_spent.append("text")
+		.attr({
+			x: 0,
+			y: 25
+			})
+		.text("Расходы");
+
+
+
+		});
+
+
+
+
+// Работа с селекторами меню
+
+function call_selected() {
+	var options =  [];
+	for (var i = 0; i < menu_oblast.length; i++) {
+		options.push(menu_oblast[i].value);
+	};
+
+
+
+var request;
+
+          if (window.XMLHttpRequest) {
+            
+            request = new XMLHttpRequest();
+          } else {  
+            request = new ActiveXObject("Microsoft.XMLHTTP");
+          }
+
+request.onreadystatechange = function() {
+            if (request.readyState == 4 && request.status == 200) {
+              
+              var data = JSON.parse(request.responseText);
+              jsonData = data;
+              
+              if (document.getElementById("output_table")) {
+				  document.getElementById("output_table").remove();
+			  };
+			  var target = document.getElementById("menu");
+			  var output_table = document.createElement("table");
+			  output_table.setAttribute("id", "output_table");
+			  var table_head = document.createElement("thead");
+			  if (data[0]['region']) {
+				  var head_row = document.createElement("tr");
+				  var headers = ["Регион", "По плану", "Уточнено", "%"];
+				  for (var a = 0; a < headers.length; a++) {
+					  var head_cell = document.createElement("td");
+					  var head_cell_data = document.createTextNode(headers[a]);
+					  head_cell.appendChild(head_cell_data);
+					  head_row.appendChild(head_cell);
+					  
+				  }
+			  } else if (data[0]['title']) {
+				var head_row = document.createElement("tr");
+				  var headers = ["Вид расходов", "По плану", "Уточнено", "%"];
+				  for (var a = 0; a < headers.length; a++) {
+					  var head_cell = document.createElement("td");
+					  var head_cell_data = document.createTextNode(headers[a]);
+					  head_cell.appendChild(head_cell_data);
+					  head_row.appendChild(head_cell);
+			  }
+		  }
+			table_head.appendChild(head_row);
+			output_table.appendChild(table_head);
+			target.appendChild(output_table);
+			  var final_row;
+			  var table_body = document.createElement("tbody");
+              for (var i = 0; i < data.length; i++) {
+
+				var body_row = document.createElement("tr");
+				var keys = Object.keys(data[0]);
+
+				for (var b = 0; b < keys.length; b++) {
+					if (keys[b] == "plan" || keys[b] == "changed") {
+						var row_cell_text = document.createTextNode(addCommas(data[i][keys[b]]));
+					} else {
+					var row_cell_text = document.createTextNode(data[i][keys[b]]);
+					}
+					var row_cell = document.createElement("td");
+					row_cell.appendChild(row_cell_text);
+					body_row.appendChild(row_cell);
+				}
+				table_body.appendChild(body_row);
+			  }
+
+				table_body.appendChild(body_row);
+			  output_table.appendChild(table_body);
+	if (data[0].region) {
+    document.getElementById('regions').getElementsByTagName('option')[0].selected = 'selected';
+ };
+            }
+          }
+          request.open("GET","api/?region=" + options[0] + "&function=" + options[1], true);
+          request.send();
+
+	};
+
+var menu_oblast = document.getElementById("menu").getElementsByTagName("select");
+//console.log(menu_oblast);
+for (var i = 0; i < menu_oblast.length; i++) {
+	menu_oblast[i].onchange = call_selected;
+	};
+
